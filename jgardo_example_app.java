3c8e10bfb3b60a7fc322c77c3c329a9011765f9b
jreleaserbot <jreleaser@kordamp.org>
2024-04-18 17:52:39 +0200

    jgardo-example-app 1.0.0-alpha2

--- jgardo_example_app.java
+++ jgardo_example_app.java
@@ -1,7 +1,7 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 17+
-//DEPS org.acme:jgardo-example-app:1.0.0-alpha2
-// Generated with JReleaser 1.11.0 at 2024-04-18T17:42:00.027213+02:00
+//DEPS command-mode-quickstart:jgardo-example-app:1.0.0-alpha2
+// Generated with JReleaser 1.11.0 at 2024-04-18T17:52:17.969054+02:00
 
 public class jgardo_example_app {
     public static void main(String... args) throws Exception {
